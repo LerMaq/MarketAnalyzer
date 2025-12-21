d6c2c7a533ba006faf1a5596a664eca58e418de8
LerMaq <kolomeichenko.d@gmail.com>
2025-12-21 20:25:07 +0700

    Перенёс в отдельную папку

--- backend/main.py
+++ backend/main.py
@@ -6,12 +6,11 @@ from router import router as products_router
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    await delete_tables()
-    print("База очищена")
     await create_tables()
     print("База создана")
     yield
     print("Выключение")
+
 
 app = FastAPI(lifespan=lifespan)
 app.include_router(products_router)
