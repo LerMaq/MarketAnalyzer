cd9511f27a330f79395e8588e74020ded8af3094
LerMaq <kolomeichenko.d@gmail.com>
2026-03-16 03:26:56 +0700

    Фикс ошибки: "POST /products/check HTTP/1.1" 422 Unprocessable Entity

--- backend/app/routes/products.py
+++ backend/app/routes/products.py
@@ -4,7 +4,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.database import get_db
 from app.services import ProductService
-from app.schemas import SProductFull, SProductVersionsList, SProductCheck, SProductTopItem
+from app.schemas import SProductFull, SProductVersionsList, SCheckProductRequest, SProductTopItem
 from app.auth.dependencies import get_current_user
 from app.models.user import User
 
@@ -16,7 +16,7 @@ async def get_top_products(db: AsyncSession = Depends(get_db)):
     return await service.get_top_products()
 
 @router.post("/check", response_model=SProductVersionsList)
-async def check_versions(data: SProductCheck, db: AsyncSession = Depends(get_db)):
+async def check_versions(data: SCheckProductRequest, db: AsyncSession = Depends(get_db)):
     service = ProductService(db)
     return await service.get_versions_list(data.url)
 
